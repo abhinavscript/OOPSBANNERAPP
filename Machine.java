87463a4b6b1ad0867419e3beec245c458c306e6c
abhinavscript <abhinavsharma10359@gmail.com>
2026-04-10 23:15:11 +0530

    java programme

---
+++ Machine.java
@@ -0,0 +1,13 @@
+public class Machine {
+     public static void main(String[] args) {
+
+        String name;
+        if (args.length > 0 && !args[0].isEmpty()) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
+
+        System.out.println("Hello " + name);
+    }
+}
